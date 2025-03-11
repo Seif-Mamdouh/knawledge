010a92f3e2393e3b925e524ad7947a4f85e31d57
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-11 12:58:26 -0400

    add nextauth with github provider, remove css vars, update button

--- components/signInButton.tsx
+++ components/signInButton.tsx
@@ -18,7 +18,7 @@ export default function SignInButton() {
 
   return (
     <button
-      onClick={() => signIn('github', { callbackUrl: '/admin' })}
+      onClick={() => signIn('github')}
       className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors"
     >
       Sign in with GitHub
