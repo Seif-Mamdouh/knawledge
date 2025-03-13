a1221df0e38756507225a316bdc01fe20c9aac96
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-13 15:43:37 -0400

    remove unnecessary comments, simplify filename logic

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -17,8 +17,7 @@ export async function POST(req: Request) {
   const filename = req.headers.get('x-vercel-filename') || 'file.txt'
   const contentType = req.headers.get('content-type') || 'text/plain'
   const fileType = `.${contentType.split('/')[1]}`
-
-  // construct final filename based on content-type if not provided
+  
   const finalName = filename.includes(fileType)
     ? filename
     : `${filename}${fileType}`
