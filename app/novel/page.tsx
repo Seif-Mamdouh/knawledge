f60345378014760f5f718b5b9e1347812a7b4515
Seif-Mamdouh <seifmamdouh7878@gmail.com>
2025-03-13 14:42:17 -0400

    refactor page component to use NovelEditor function

--- app/novel/page.tsx
+++ app/novel/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import { useEditor } from "novel";
 import Editor from "@/components/noveleditor/editor";
 
-import { useEditor } from "novel";
-
 export default function Page() {
-  const { editor } = useEditor();
-
   return (
-    <div>
+      <NovelEditor />
+  );
+}
+
+function NovelEditor() {
+  const { editor } = useEditor();
+  
+  return (
+    <div className="p-4">
       <Editor initialValue={editor?.getJSON()} onChange={() => {}} />
     </div>
   );
